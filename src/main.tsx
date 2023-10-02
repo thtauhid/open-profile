a5828f49f20448597b8ca49210c4f4c2ce33f0b1
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2023-10-02 21:11:29 +0530

    Added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./main.css";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
