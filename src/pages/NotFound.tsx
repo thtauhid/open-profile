59584c9cb4c6716dea17ae8c2cea022acf773681
milinddethe15 <milinddethe15@gmail.com>
2023-10-02 23:08:54 +0530

    fixes: #13

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="h-screen flex items-center justify-center flex-col">
+    <h1 className="font-bold text-4xl text-center">404 - Page Not Found</h1>;
+    </div>
+  )
+};
+
+export default NotFound;
