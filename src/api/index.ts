10a909aed64a380f0fe66225772244838190eae6
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2023-10-02 22:59:51 +0530

    Added API to Bio.tsx page

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,15 @@
+import info from "../../data/johndoe.json";
+
 export const getFullName = () => {
   return "John Doe";
 };
 
 export const getInitials = () => {
   return "JD";
+};
+
+export const getIntro = () => {
+  return info.intro;
 };
 
 export const isAvailableForHire = () => {
