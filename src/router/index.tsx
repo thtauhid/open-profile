1fc26a420d6f2732685ef77dc5a23c371685b2a8
Ryan Maddock <ryanmaddock16@gmail.com>
2023-10-02 22:28:10 +0100

    #27 - Explore Page
    Created new explore page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Homepage from "@/pages/Homepage";
 import Profile from "@/pages/Profile";
 import NotFound from "@/pages/NotFound";
+import Explore from "@/pages/Explore";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
   {
     path: "profile/:username",
     element: <Profile />,
+  },
+  {
+    path: "/explore",
+    element: <Explore />,
   },
   {
     path: "*", 
