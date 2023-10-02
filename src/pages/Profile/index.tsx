4b9ea4c54140f4fc19dfe940b70bfbb6522bf747
Kinshu-Learner <kinshurajgupta1@gmail.com>
2023-10-02 21:45:49 +0530

    Added a simple bio/intro component in pages

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,5 +1,7 @@
 // import { useParams } from "react-router-dom";
+import Bio from "./Bio";
 import Header from "./Header";
+
 
 function Profile() {
   // const { username } = useParams();
@@ -7,6 +9,7 @@ function Profile() {
     <div className="flex justify-center my-10">
       <div className="w-1/2">
         <Header />
+        <Bio />
       </div>
     </div>
   );
