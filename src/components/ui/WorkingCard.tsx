2de9433b6d8a9a0c933975c742d9acc96c1a895d
Shivampatil22 <122476829+Shivampatil22@users.noreply.github.com>
2023-10-03 02:15:51 +0530

    Added new Workingon Component

---
+++ src/components/ui/WorkingCard.tsx
@@ -0,0 +1,18 @@
+
+
+const WorkingCard = ({
+  title,
+  content,
+}: {
+  title: string;
+  content: string;
+}) => {
+  return (
+    <article className="lg:w-[46%]  w-full  flex flex-col p-5 bg-slate-200 my-2 mx-1 justify-around rounded-lg border-black border-solid border-2">
+        <div className="my-2 font-bold text-2xl">{title}</div>
+        <div className="font-medium text-md">{content}</div>
+    </article>
+  );
+};
+
+export default WorkingCard;
---
+++ src/pages/Profile/Working.tsx
@@ -0,0 +1,44 @@
+import WorkingCard from "@/components/ui/WorkingCard";
+import { Button } from "@/components/ui/button";
+
+const Working = () => {
+  return (
+    <div className="border-2 flex justify-between items-center flex-col p-3">
+      <div className="flex w-full justify-start my-2 text-[2rem] font-bold pl-7">
+        Working On
+      </div>
+      <section className="flex flex-wrap justify-around w-full px-2">
+        <WorkingCard
+          title={"Web Development"}
+          content={
+            " Lorem ipsum dolor sit amet consectetur adipisicing elit. Et error nisi in est corrupti nesciunt. Consequatur saepe nulla incidunt culpa. Sit saepe nemo iste maxime vero nihil quis aliquam officiis?"
+          }
+        />
+        <WorkingCard
+          title={"Web Development"}
+          content={
+            " Lorem ipsum dolor sit amet consectetur adipisicing elit. Et error nisi in est corrupti nesciunt. Consequatur saepe nulla incidunt culpa. Sit saepe nemo iste maxime vero nihil quis aliquam officiis?"
+          }
+        />
+        <WorkingCard
+          title={"Web Development"}
+          content={
+            " Lorem ipsum dolor sit amet consectetur adipisicing elit. Et error nisi in est corrupti nesciunt. Consequatur saepe nulla incidunt culpa. Sit saepe nemo iste maxime vero nihil quis aliquam officiis?"
+          }
+        />
+        <WorkingCard
+          title={"Web Development"}
+          content={
+            " Lorem ipsum dolor sit amet consectetur adipisicing elit. Et error nisi in est corrupti nesciunt. Consequatur saepe nulla incidunt culpa. Sit saepe nemo iste maxime vero nihil quis aliquam officiis?"
+          }
+        />
+      </section>
+
+      <div className="my-4 flex w-full justify-end pr-5">
+        <Button variant={"blackwhite"}>More</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Working;
