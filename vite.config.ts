3f943d594ec81a0aa4482272d2f8583a44ea23df
Robert Lupas <robertlupas09@gmail.com>
2023-10-03 16:55:41 +0300

    Included necessary files in Vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { VitePWA } from "vite-plugin-pwa";
+import PWAmanifest from './manifest.json';
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      includeAssets: ['/src/assets/icons/favicon.ico', '/src/assets/icons/apple-touch-icon.png'],
       registerType: 'autoUpdate',
+      manifest: PWAmanifest,
       devOptions: {
         enabled: true
       }
