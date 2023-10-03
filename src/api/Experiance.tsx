899840ab7d1193f4e1ae7366c500ae8201dae717
SAGARSINGH-1 <sagar01.ko@gmail.com>
2023-10-03 17:33:25 +0530

    Added A Experiance Co

---
+++ src/api/Experiance.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@/components/ui/button";
+import {getExperience} from '@/api/index'
+
+export default function Experiance() {
+
+    const data =getExperience();
+    console.log(data);
+
+    return (
+        <div className="flex p-[30px] justify-center items-center">
+            <div className="p-[10px] w-[100%] border-2 border-black">
+                <h1 className="text-2xl font-bold">Experiance</h1>
+                <div className="flex p-[30px] flex-wrap justify-center items-center gap-10">
+                    {data.map((data) => {
+                        return (
+                            <div className="border-2 border-black w-[45%] p-2 rounded-md">
+                                <div className="p-1">
+                                    <h1 className="text-2xl font-bold">{data.title}</h1>
+                                    <p className="text-gray-500">{data.company}</p>
+                                    <div className="">
+                                    <span>Location : </span>
+                                    <span>{data.location}</span>
+                                    </div>
+                                    <div className="px-1 flex justify-between">
+                                        <p>{data.start_date}</p>
+                                        <p>{data.end_date}</p>
+                                    </div>
+                                    <p>{data.description}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
+
+                </div>
+                <div className="flex justify-end px-10 py-1">
+                    <Button type="submit">More</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
