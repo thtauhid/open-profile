d281c36a20e27e0350f071dc4cd93f24c6e7a50d
Robert Lupas <robertlupas09@gmail.com>
2023-10-04 23:19:24 +0300

    Removed unnecessary folder paths and used double quotation marks

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { VitePWA } from "vite-plugin-pwa";
-import PWAmanifest from './manifest.json';
+import PWAmanifest from "./manifest.json";
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      includeAssets: ['/public/icons/favicon.ico', '/public/icons/apple-touch-icon.png'],
-      registerType: 'autoUpdate',
+      includeAssets: ["/icons/favicon.ico", "/icons/apple-touch-icon.png"],
+      registerType: "autoUpdate",
       manifest: PWAmanifest,
       devOptions: {
-        enabled: true
-      }
-    })
+        enabled: true,
+      },
+    }),
   ],
   resolve: {
     alias: {
