64c63dca75c89010d3d228e092b66a4939a84757
Vidit Agrawal <viditagrawal56@gmail.com>
2023-10-05 01:04:58 +0530

    fixes : #5

---
+++ src/pages/Profile/TopRepo.tsx
@@ -0,0 +1,31 @@
+import { getTopRepos } from "@/api";
+import { Button } from "@/components/ui/button";
+
+const TopRepo = () => {
+  const data = getTopRepos();
+  console.log(data);
+
+  return (
+    <div className="p-[10px] my-8 border-2 border-gray">
+      <h1 className="text-3xl p-[10px] pl-[25px] font-bold">Top Repo</h1>
+      <div className="lg:flex p-[10px] pl-[25px] flex-col gap-10">
+        {data.map((data) => {
+          return (
+            <div className="border-2 border-black lg:w-full p-2 m-2 lg:m-0 rounded-md">
+              <div className="p-1">
+                <h1 className="text-2xl font-bold">{data.title}</h1>
+                <p className="text-gray-500">{data.description}</p>
+                <p className="text-gray-500">{data.url}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="flex justify-end px-10 py-1">
+        <Button type="submit">More</Button>
+      </div>
+    </div>
+  );
+};
+
+export default TopRepo;
