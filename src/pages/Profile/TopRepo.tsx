c7e5070889c7605dbd8659393214b9fd52d0456a
Vidit Agrawal <viditagrawal56@gmail.com>
2023-10-05 01:27:00 +0530

    fixes : #5

--- src/pages/Profile/TopRepo.tsx
+++ src/pages/Profile/TopRepo.tsx
@@ -6,8 +6,8 @@ const TopRepo = () => {
   console.log(data);
 
   return (
-    <div className="p-[10px] my-8 border-2 border-gray">
-      <h1 className="text-3xl p-[10px] pl-[25px] font-bold">Top Repo</h1>
+    <div className="p-[25px] my-8 border-2 border-gray">
+      <h1 className="text-3xl p-[10px] pl-[25px] font-bold">Top Repos</h1>
       <div className="lg:flex p-[10px] pl-[25px] flex-col gap-10">
         {data.map((data) => {
           return (
@@ -21,7 +21,7 @@ const TopRepo = () => {
           );
         })}
       </div>
-      <div className="flex justify-end px-10 py-1">
+      <div className="flex justify-end px-3 py-1">
         <Button type="submit">More</Button>
       </div>
     </div>
