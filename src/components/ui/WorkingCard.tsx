1a2e0145f35d7334fc7dbc72ce01aa8908588022
Hritik <hritikadhikari05@gmail.com>
2023-10-10 23:18:36 +0530

    fixed Working card title font

--- src/components/ui/WorkingCard.tsx
+++ src/components/ui/WorkingCard.tsx
@@ -7,7 +7,7 @@ const WorkingCard = ({
 }) => {
   return (
     <article className="lg:w-[46%]  w-full  flex flex-col p-5  my-2  justify-around rounded-lg border-solid border-2">
-      <div className="my-2 font-bold text-2xl">{title}</div>
+      <div className="font-bold text-xl">{title}</div>
       <div className="font-medium text-gray-500 text-md">{content}</div>
     </article>
   );
