64e6146eb5188de9bfd76e8364cf804d9d56ccc6
Hritik <hritikadhikari05@gmail.com>
2023-10-10 23:19:39 +0530

    Fixed Wiorking card sub-title text

--- src/components/ui/WorkingCard.tsx
+++ src/components/ui/WorkingCard.tsx
@@ -8,7 +8,7 @@ const WorkingCard = ({
   return (
     <article className="lg:w-[46%]  w-full  flex flex-col p-5  my-2  justify-around rounded-lg border-solid border-2">
       <div className="font-bold text-xl">{title}</div>
-      <div className="font-medium text-gray-500 text-md">{content}</div>
+      <div className=" text-gray-500 text-md">{content}</div>
     </article>
   );
 };
